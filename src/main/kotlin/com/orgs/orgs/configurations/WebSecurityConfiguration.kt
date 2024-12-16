fb11cb77d4a6a91dc833ebfc9c514bc8e7b78303
Kai Wang <kaiuchihaa@gmail.com>
2024-12-16 20:48:48 -0300

    test

--- src/main/kotlin/com/orgs/orgs/configurations/WebSecurityConfiguration.kt
+++ src/main/kotlin/com/orgs/orgs/configurations/WebSecurityConfiguration.kt
@@ -5,9 +5,10 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.core.userdetails.User
-import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.security.provisioning.InMemoryUserDetailsManager
 import org.springframework.security.web.SecurityFilterChain
+
 
 @Configuration
 @EnableWebSecurity
@@ -23,15 +24,15 @@ class WebSecurityConfiguration {
     }
 
     @Bean
-    fun userDetailsService(): UserDetailsService{
-        val user = User.withDefaultPasswordEncoder()
+    fun userDetailsService(): InMemoryUserDetailsManager {
+        val user: UserDetails = User.withDefaultPasswordEncoder()
             .username("kaiwang")
             .password("123")
             .roles("USER")
             .build()
 
+        // Agora estamos passando o UserDetails diretamente para o InMemoryUserDetailsManager
         return InMemoryUserDetailsManager(user)
-
     }
 
 }
