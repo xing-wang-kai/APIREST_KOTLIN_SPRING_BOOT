21522c76cb95d9a15a702892aef40bf2bc4f4e9d
Kai Wang <kaiuchihaa@gmail.com>
2024-12-17 16:31:45 -0300

    add

--- src/main/kotlin/com/orgs/orgs/controller/handlers/ErrorHandlers.kt
+++ src/main/kotlin/com/orgs/orgs/controller/handlers/ErrorHandlers.kt
@@ -1,6 +1,7 @@
 package com.orgs.orgs.controller.handlers
 
-import javax.servlet.http.HttpServletRequest
+
+import jakarta.servlet.http.HttpServletRequest
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ControllerAdvice
--- src/main/kotlin/com/orgs/orgs/models/Products.kt
+++ src/main/kotlin/com/orgs/orgs/models/Products.kt
@@ -1,9 +1,9 @@
 package com.orgs.orgs.models
 
-import javax.persistence.Entity
-import javax.persistence.GeneratedValue
-import javax.persistence.Id
-import javax.persistence.Column
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.Id
 import java.math.BigDecimal
 
 
